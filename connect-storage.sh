7e65b15b00d7d6cae3499ed02b071994cc72f5fd
olegbogomaz <33133022+olegbogomaz@users.noreply.github.com>
2023-08-24 10:18:20 -0400

    Create connect-storage.sh

---
+++ connect-storage.sh
@@ -0,0 +1,12 @@
+RESOURCE_GROUP_NAME="rg-func-dev-canc-001"
+STORAGE_ACCOUNT_NAME="rgfuncdevcanc001b734"
+
+# This command assumes you have logged in with az login
+HTTP_ENDPOINT=$(az storage account show \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $STORAGE_ACCOUNT_NAME \
+    --query "primaryEndpoints.file" --output tsv | tr -d '"')
+SMBPATH=$(echo $HTTP_ENDPOINT | cut -c7-${#HTTP_ENDPOINT})
+FILE_HOST=$(echo $SMBPATH | tr -d "/")
+
+nc -zvw3 $FILE_HOST 445
