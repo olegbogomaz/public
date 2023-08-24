12ca47d15ad00ba25e5fba599fa8dbf5476f6b55
olegbogomaz <33133022+olegbogomaz@users.noreply.github.com>
2023-08-24 10:22:26 -0400

    Update connect-storage.sh

--- connect-storage.sh
+++ connect-storage.sh
@@ -1,12 +1,12 @@
-RESOURCE_GROUP_NAME="rg-func-dev-canc-001"
-STORAGE_ACCOUNT_NAME="rgfuncdevcanc001b734"
+sudo mkdir /mnt/oleg
+if [ ! -d "/etc/smbcredentials" ]; then
+sudo mkdir /etc/smbcredentials
+fi
+if [ ! -f "/etc/smbcredentials/rgfuncdevcanc001b734.cred" ]; then
+    sudo bash -c 'echo "username=rgfuncdevcanc001b734" >> /etc/smbcredentials/rgfuncdevcanc001b734.cred'
+    sudo bash -c 'echo "password=SO6+gx8mqaN1KBEx1ai3+aweDxESte8ri/8d3MqoC6Zek4mR1Yx2f9sy7vfWLH9taqwxBhb5IE6n+AStiewHsg==" >> /etc/smbcredentials/rgfuncdevcanc001b734.cred'
+fi
+sudo chmod 600 /etc/smbcredentials/rgfuncdevcanc001b734.cred
 
-# This command assumes you have logged in with az login
-HTTP_ENDPOINT=$(az storage account show \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --name $STORAGE_ACCOUNT_NAME \
-    --query "primaryEndpoints.file" --output tsv | tr -d '"')
-SMBPATH=$(echo $HTTP_ENDPOINT | cut -c7-${#HTTP_ENDPOINT})
-FILE_HOST=$(echo $SMBPATH | tr -d "/")
-
-nc -zvw3 $FILE_HOST 445
+sudo bash -c 'echo "//rgfuncdevcanc001b734.file.core.windows.net/oleg /mnt/oleg cifs nofail,credentials=/etc/smbcredentials/rgfuncdevcanc001b734.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab'
+sudo mount -t cifs //rgfuncdevcanc001b734.file.core.windows.net/oleg /mnt/oleg -o credentials=/etc/smbcredentials/rgfuncdevcanc001b734.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
